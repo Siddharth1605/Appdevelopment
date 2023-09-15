652adbca3c9cac637ea7658d375a31cc4e0ff628
Siddharth1605 <Siddhashokan@gmail.com>
2023-09-15 08:03:59 +0530

    Day2 and Day3 tasks are completed.

---
+++ FrontEnd/day3/src/components/Login.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './login.css';
+export default function Login() {
+  return (
+    <>
+    <div className="min-h-screen flex items-center justify-center sid">
+      <div className="custom-gradient shadow-md rounded-lg p-8 mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32">
+        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="/signup" >
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6">
+          </input>
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          <div class="text-sm">
+            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+          </div>
+        </div>
+        <div class="mt-2">
+        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-purple-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">Sign in</button>
+      </div>
+    </form>
+
+    <p class="mt-10 text-center text-sm text-black-500">
+      Dont have an account?
+      <a href="/signup" class="font-semibold leading-6 text-indigo-600 hover:text-purple-500">Sign up here</a>
+    </p>
+  </div>
+</div>
+</div>
+    </div>
+    </>
+  )
+}
